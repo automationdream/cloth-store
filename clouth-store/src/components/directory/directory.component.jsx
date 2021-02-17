b73140bd99338e5a57326549b30676f7f702075b
Bartosz Tyrala <automationdream@gmail.com>
2021-02-17 20:44:47 +0100

    [refactor] Initialize class Directory which extends React.Component and inherits its functions

--- clouth-store/src/components/directory/directory.component.jsx
+++ clouth-store/src/components/directory/directory.component.jsx
@@ -1,3 +1,14 @@
 import React from "react";
 
 import './directory.styles.scss'
+
+class Directory extends React.Component {
+    constructor() {
+        super();
+
+    }
+
+
+}
+
+export default Directory;
