554aa091a9616d34859599d20e8666c26b38d8cc
Bartosz Tyrala <automationdream@gmail.com>
2021-02-17 21:20:30 +0100

    [fix] Links were not working

--- clouth-store/src/components/directory/directory.component.jsx
+++ clouth-store/src/components/directory/directory.component.jsx
@@ -12,32 +12,32 @@ class Directory extends React.Component {
             sections: [
                 {
                     title: 'hats',
-                    imageurl: 'https://i.ibb.co/cvpntl1/hats.png',
+                    imageurl: 'https://cdn.pixabay.com/photo/2016/11/29/03/15/back-1867009_960_720.jpg',
                     id: 1,
                     linkurl: 'shop/hats'
                 },
                 {
                     title: 'jackets',
-                    imageurl: 'https://i.ibb.co/px2tcc3/jackets.png',
+                    imageurl: 'https://cdn.pixabay.com/photo/2016/11/29/01/34/fashion-1866572_960_720.jpg',
                     id: 2,
                     linkurl: 'shop/jackets'
                 },
                 {
                     title: 'sneakers',
-                    imageurl: 'https://i.ibb.co/0jqhpnp/sneakers.png',
+                    imageurl: 'https://media.istockphoto.com/photos/men-wearing-sneakers-picture-id695460988?b=1&k=6&m=695460988&s=170667a&w=0&h=eL7xdtkgFMvcSX8OmYTAnWNlI9dGFBOTzFY4uZpCjdc=',
                     id: 3,
                     linkurl: 'shop/sneakers'
                 },
                 {
                     title: 'womens',
-                    imageurl: 'https://i.ibb.co/gccdy8t/womens.png',
+                    imageurl: 'https://cdn.pixabay.com/photo/2018/01/15/07/52/portrait-3083402_960_720.jpg',
                     size: 'large',
                     id: 4,
                     linkurl: 'shop/womens'
                 },
                 {
                     title: 'mens',
-                    imageurl: 'https://i.ibb.co/r70vbrq/men.png',
+                    imageurl: 'https://cdn.pixabay.com/photo/2016/01/19/18/04/man-1150058_960_720.jpg',
                     size: 'large',
                     id: 5,
                     linkurl: 'shop/mens'
