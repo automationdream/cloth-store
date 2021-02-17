50ecdf92625e068b570d809add7c1a05b1d260b3
Bartosz Tyrala <automationdream@gmail.com>
2021-02-17 20:45:32 +0100

    Add states for components

--- clouth-store/src/components/directory/directory.component.jsx
+++ clouth-store/src/components/directory/directory.component.jsx
@@ -6,6 +6,42 @@ class Directory extends React.Component {
     constructor() {
         super();
 
+        this.state = {
+            sections: [
+                {
+                    title: 'hats',
+                    imageurl: 'https://i.ibb.co/cvpntl1/hats.png',
+                    id: 1,
+                    linkurl: 'shop/hats'
+                },
+                {
+                    title: 'jackets',
+                    imageurl: 'https://i.ibb.co/px2tcc3/jackets.png',
+                    id: 2,
+                    linkurl: 'shop/jackets'
+                },
+                {
+                    title: 'sneakers',
+                    imageurl: 'https://i.ibb.co/0jqhpnp/sneakers.png',
+                    id: 3,
+                    linkurl: 'shop/sneakers'
+                },
+                {
+                    title: 'womens',
+                    imageurl: 'https://i.ibb.co/gccdy8t/womens.png',
+                    size: 'large',
+                    id: 4,
+                    linkurl: 'shop/womens'
+                },
+                {
+                    title: 'mens',
+                    imageurl: 'https://i.ibb.co/r70vbrq/men.png',
+                    size: 'large',
+                    id: 5,
+                    linkurl: 'shop/mens'
+                }
+            ]
+        }
     }
 
 
