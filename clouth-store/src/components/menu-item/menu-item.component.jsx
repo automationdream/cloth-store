5ac69fdbc4970497833ec32df43517e3d6f6f490
Bartosz Tyrala <automationdream@gmail.com>
2021-02-18 17:45:05 +0100

    Adding hover effect to menu item

--- clouth-store/src/components/menu-item/menu-item.component.jsx
+++ clouth-store/src/components/menu-item/menu-item.component.jsx
@@ -2,9 +2,10 @@ import React from "react";
 
 import './menu-item.styles.scss'
 
-const MenuItem = ({ title, subtitle, imageURL, size}) => (
+const MenuItem = ({title, subtitle, imageURL, size}) => (
 
-    <div style={{ backgroundImage: `url(${imageURL})`}} className={`${size} menu-item`}>
+    <div className={`${size} menu-item`}>
+        <div style={{backgroundImage: `url(${imageURL})`}} className='background-image'/>
         <div className='content'>
             <h1 className='title'>{title.toUpperCase()}</h1>
             <span className='subtitle'>{subtitle.toUpperCase()}</span>
