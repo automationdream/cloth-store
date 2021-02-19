e3c022bc98323bcb83af9033248ba0cd49d4d753
Bartosz Tyrala <automationdream@gmail.com>
2021-02-19 23:20:39 +0100

    Parsing parameters from url

--- clouth-store/src/App.js
+++ clouth-store/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component"
-import { Route } from 'react-router-dom';
+import {Route} from 'react-router-dom';
 
 const HatsPage = () => (
     <div>
@@ -8,13 +8,29 @@ const HatsPage = () => (
     </div>
 );
 
-function App() {
-  return (
+const TopicsList = () => (
     <div>
-        <Route exact path='/' component={ HomePage } />;
-        <Route path='/hats' component={ HatsPage } />;
+        <h1> TopicsList </h1>
     </div>
-  );
+)
+
+const TopicsDetail = (params) => (
+    <div>
+        {console.log(params)}
+        <h1> Topics Detail: {params.match.params.topicId} </h1>
+    </div>
+)
+
+
+function App() {
+    return (
+        <div>
+            <Route exact path='/' component={HomePage}/>
+            <Route path='/hats' component={HatsPage}/>
+            <Route exact path='/topics' component={TopicsList}/>
+            <Route exact path='/topics/:topicId' component={TopicsDetail}/>
+        </div>
+    );
 }
 
 export default App;
