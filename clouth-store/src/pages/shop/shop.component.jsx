8839169cc5b256a52a061a6087bb84c57aa09fb3
Bartosz Tyrala <automationdream@gmail.com>
2021-02-23 21:20:53 +0100

    Render PreviewCollection
    * destructuring collections
    * mapping collection to PreviewCollection and parse variables as id and other collection props

--- clouth-store/src/pages/shop/shop.component.jsx
+++ clouth-store/src/pages/shop/shop.component.jsx
@@ -11,7 +11,14 @@ class ShopPage extends React.Component {
     }
 
     render() {
-        return <div>SHOP PAGE</div>
+        const { collections }  = this.state;
+        return (<div className='shop-page'>
+            {
+                collections.map(({id, ...otherCollectionProps}) => (
+                <PreviewCollection key={id} {...otherCollectionProps} />
+                ))
+            }
+        </div>);
     }
 }
 
