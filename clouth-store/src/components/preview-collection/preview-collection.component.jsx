c4781495e3266916ca57f06bc162eefdf13dc228
Bartosz Tyrala <automationdream@gmail.com>
2021-02-25 21:43:23 +0100

    Parsing collection item into PreviewCollection

--- clouth-store/src/components/preview-collection/preview-collection.component.jsx
+++ clouth-store/src/components/preview-collection/preview-collection.component.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import CollectionItem from "../collection-item/collection-item.component";
 
 import './preview-collection.styles.scss'
 
@@ -10,8 +11,8 @@ const PreviewCollection = ({title, items}) => (
             {
                 items
                     .filter((item, idx) => idx < 4)
-                    .map((item) => (
-                    <div id={item.id}>{item.name}</div>
+                    .map(({id, ...itemProps}) => (
+                        <CollectionItem key={id} {...itemProps}/>
                 ))
             }
         </div>
