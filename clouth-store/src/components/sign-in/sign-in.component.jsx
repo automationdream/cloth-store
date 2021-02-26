58cd89af169d26603598ba085116cb534cf6ce02
Bartosz Tyrala <automationdream@gmail.com>
2021-02-26 14:19:46 +0100

    Handling onChange

--- clouth-store/src/components/sign-in/sign-in.component.jsx
+++ clouth-store/src/components/sign-in/sign-in.component.jsx
@@ -20,6 +20,14 @@ class SignIn extends React.Component {
         })
     }
 
+    handleChange = event => {
+        const {value, name} = event.target;
+
+        this.setState({[name]: value})
+
+
+    }
+
     render() {
         return (
             <div className='sign-in'>
@@ -27,9 +35,9 @@ class SignIn extends React.Component {
                 <span>Sign in with your email and password</span>
 
                 <form onSubmit={this.handleSubmit}>
-                    <input name='email' type='email' value={this.state.email}  required/>
+                    <input name='email' type='email' value={this.state.email} onChange={this.handleChange} required/>
                     <label>Email</label>
-                    <input name='password' type='password' value={this.state.password}
+                    <input name='password' type='password' value={this.state.password} onChange={this.handleChange}
                            required/>
                     <label>Password</label>
                     <input type='submit' value='Submit form'/>
