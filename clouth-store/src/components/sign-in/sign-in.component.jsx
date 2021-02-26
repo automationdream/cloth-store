5a5d774b6313469d7100a3e390ad0eaf9263a479
Bartosz Tyrala <automationdream@gmail.com>
2021-02-26 21:38:18 +0100

    Attaching Custom Button for submit field

--- clouth-store/src/components/sign-in/sign-in.component.jsx
+++ clouth-store/src/components/sign-in/sign-in.component.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import './sign-in.component.scss'
 
 import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
 
 class SignIn extends React.Component {
     constructor(props) {
@@ -43,7 +44,7 @@ class SignIn extends React.Component {
                                handleChange={this.handleChange}
                                label='Password'
                                required/>
-                    <input type='submit' value='Submit form'/>
+                    <CustomButton type='submit'> Sign in </CustomButton>
                 </form>
             </div>
         )
