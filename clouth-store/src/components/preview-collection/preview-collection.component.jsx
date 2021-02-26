e02b5793006fc9408c4ed202ec8a1adf0d70dff4
Bartosz Tyrala <automationdream@gmail.com>
2021-02-26 01:32:07 +0100

    Fix- Add <div/> around elements

--- clouth-store/src/components/preview-collection/preview-collection.component.jsx
+++ clouth-store/src/components/preview-collection/preview-collection.component.jsx
@@ -5,16 +5,18 @@ import './preview-collection.styles.scss'
 
 
 const PreviewCollection = ({title, items}) => (
-    <div>
-        <h1 className='title'>{title.toUpperCase()}</h1>
-        <div className='preview'>
-            {
-                items
-                    .filter((item, idx) => idx < 4)
-                    .map(({id, ...itemProps}) => (
-                        <CollectionItem key={id} {...itemProps}/>
-                ))
-            }
+    <div className='collection-preview'>
+        <div>
+            <h1 className='title'>{title.toUpperCase()}</h1>
+            <div className='preview'>
+                {
+                    items
+                        .filter((item, idx) => idx < 4)
+                        .map(({id, ...itemProps}) => (
+                            <CollectionItem key={id} {...itemProps}/>
+                        ))
+                }
+            </div>
         </div>
     </div>
 );
