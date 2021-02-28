aa0e5cbe7ecb6b97fa0b8dbe2b222205d3d64036
Bartosz Tyrala <automationdream@gmail.com>
2021-02-28 15:17:58 +0100

    Add parameter isGoogleSignIn

--- clouth-store/src/components/sign-in/sign-in.component.jsx
+++ clouth-store/src/components/sign-in/sign-in.component.jsx
@@ -5,7 +5,7 @@ import './sign-in.component.scss'
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 
-import { signInWithGoogle } from "../firebase/firebase.utils";
+import {signInWithGoogle} from "../firebase/firebase.utils";
 
 class SignIn extends React.Component {
     constructor(props) {
@@ -46,8 +46,10 @@ class SignIn extends React.Component {
                                handleChange={this.handleChange}
                                label='Password'
                                required/>
-                    <CustomButton type='submit'> Sign in </CustomButton>
-                    <CustomButton onClick={signInWithGoogle}> Sign in with Google </CustomButton>
+                    <div className='buttons'>
+                        <CustomButton type='submit'> Sign in </CustomButton>
+                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn> Sign in with Google </CustomButton>
+                    </div>
                 </form>
             </div>
         )
