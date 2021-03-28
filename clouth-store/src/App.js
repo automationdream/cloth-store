d5276a6568b35800f7b571500d6e401e65d88a83
Bartosz Tyrala <automationdream@gmail.com>
2021-03-28 13:13:51 +0200

    Create User Profile Document if user does not exist

--- clouth-store/src/App.js
+++ clouth-store/src/App.js
@@ -5,7 +5,7 @@ import {Switch, Route} from 'react-router-dom';
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import { auth } from "./components/firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./components/firebase/firebase.utils";
 
 class App extends React.Component {
     constructor() {
@@ -19,7 +19,7 @@ class App extends React.Component {
 
     componentDidMount() {
         this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-            this.setState({currentUser: user})
+            createUserProfileDocument(user);
         })
     }
 
