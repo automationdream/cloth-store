d72f306e741337eb6fda1bdfecb0a678a08adc4d
Bartosz Tyrala <automationdream@gmail.com>
2021-03-28 16:11:25 +0200

    Fix display

--- clouth-store/src/components/sign-up/sign-up.component.jsx
+++ clouth-store/src/components/sign-up/sign-up.component.jsx
@@ -82,7 +82,7 @@ class SignUp extends React.Component {
                         name='confirmPassword'
                         value={confirmPassword}
                         onChange={this.handleChange}
-                        label='confirmPassword'
+                        label='Confirm Password'
                     />
                     <CustomButton type='submit'>SIGN UP</CustomButton>
                 </form>
