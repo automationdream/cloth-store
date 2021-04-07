8704bff15906f2903e1e25f0672f12d1b3061b92
Bartosz Tyrala <automationdream@gmail.com>
2021-04-07 20:34:52 +0200

    using selector for currentUser and hidden

--- clouth-store/src/components/header/header.component.jsx
+++ clouth-store/src/components/header/header.component.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import './header-component.styles.scss'
-import { Link } from 'react-router-dom';
-import { ReactComponent as Logo } from '../../assets/crown.svg'
-import { auth } from "../firebase/firebase.utils";
+import {Link} from 'react-router-dom';
+import {ReactComponent as Logo} from '../../assets/crown.svg'
+import {auth} from "../firebase/firebase.utils";
 import CartIcon from "../cart-icon/cart-icon.component";
 import CartDropdown from "../cart-dropdown/cart-dropdown.component";
-import { connect} from "react-redux";
+import {connect} from "react-redux";
+import {createStructuredSelector} from "reselect";
+import {selectCartHidden, selectCurrentUser} from "../../redux/user/user.selectors";
 
-const Header = ({ currentUser, hidden }) => (
+const Header = ({currentUser, hidden}) => (
     <div className='header'>
         <Link className='logo-container' to='/'>
             <Logo className='logo'/>
@@ -17,7 +19,7 @@ const Header = ({ currentUser, hidden }) => (
             <Link className='option' to='/contact'>CONTACT</Link>
             {
                 currentUser ?
-                    <div className='option' onClick={()=>auth.signOut()}>SIGN-OUT</div>
+                    <div className='option' onClick={() => auth.signOut()}>SIGN-OUT</div>
                     :
                     <Link className='option' to='/sign-in'>SIGN-IN</Link>
             }
@@ -29,9 +31,9 @@ const Header = ({ currentUser, hidden }) => (
     </div>
 )
 
-const mapStateToProps = ({user: {currentUser},  cart: {hidden}}) => ({
-    currentUser,
-    hidden
+const mapStateToProps = (state) => ({
+    currentUser: selectCurrentUser(state),
+    hidden: selectCartHidden(state)
 })
 
 export default connect(mapStateToProps)(Header);
