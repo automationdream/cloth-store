520dba7198224534e60454ae571331cac8342a71
Bartosz Tyrala <automationdream@gmail.com>
2021-04-07 23:40:24 +0200

    Register new path to checkout

--- clouth-store/src/App.js
+++ clouth-store/src/App.js
@@ -4,6 +4,7 @@ import HomePage from "./pages/homepage/homepage.component"
 import {Switch, Route, Redirect} from 'react-router-dom';
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
+import CheckoutPage from "./pages/checkout/checkout.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import {auth, createUserProfileDocument} from "./components/firebase/firebase.utils";
 import {setCurrentUser} from "./redux/user/user.actions";
@@ -53,6 +54,7 @@ class App extends React.Component {
                     <Route path='/shop' component={ShopPage}/>
                     <Route exact path='/sign-in'
                            render={() => this.props.currentUser ? (<Redirect to='/'/>) : (<SignInAndSignUpPage/>)}/>
+                    <Route exact path='/checkout' component={CheckoutPage}/>
                 </Switch>
             </div>
         )
