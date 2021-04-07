05de717e8e06c56d19095aba074e0b86fcd66f6b
Bartosz Tyrala <automationdream@gmail.com>
2021-04-07 20:12:03 +0200

    use memoized function for calculating shop items

--- clouth-store/src/components/cart-icon/cart-icon.component.jsx
+++ clouth-store/src/components/cart-icon/cart-icon.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {connect} from "react-redux";
 import {toggleCartHidden} from "../../redux/cart/cart.actions";
+import {selectCartItemsCount} from "../../redux/cart/cart.selectors";
 
 
 import {ReactComponent as ShippingIcon} from '../../assets/shopping-bag.svg'
@@ -18,8 +19,8 @@ const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-const mapStateToProps = ({cart: {cartItems}}) => ({
-    itemsCount: cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0)
+const mapStateToProps = (state) => ({
+    itemsCount: selectCartItemsCount(state)
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
