00790a43ff2c337f0b5507e3a83ba033eaa494ec
Bartosz Tyrala <automationdream@gmail.com>
2021-04-07 23:41:19 +0200

    Right now click on the button has access to history.push method

--- clouth-store/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ clouth-store/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -8,15 +8,22 @@ import {selectCartItems} from "../../redux/cart/cart.selectors";
 import {createStructuredSelector} from "reselect";
 import { withRouter } from 'react-router-dom'
 
-const CartDropdown = ({cartItems}) => (
-    <div className='cart-dropdown'>
-        <div className='cart-items'>
-            {
-                cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem}/>)
-            }
-        </div>
-        <CustomButton>CHECKOUT</CustomButton>
-    </div>
+const CartDropdown = ({cartItems, history}) => (
+    cartItems.length ?
+        (<div className='cart-dropdown'>
+            <div className='cart-items'>
+                {
+                    cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem}/>)
+                }
+            </div>
+            <CustomButton onClick={() => history.push('/checkout')}>CHECKOUT</CustomButton>
+        </div>)
+        :
+        (<div className='cart-dropdown'>
+            <div className='empty-message'>
+                Nothing in the basket
+            </div>
+        </div>)
 )
 
 const mapStateToProps = createStructuredSelector({cartItems: selectCartItems})
