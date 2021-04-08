2e0c7b258eb6121e8df29211fcd26821d91b207f
Bartosz Tyrala <automationdream@gmail.com>
2021-04-08 23:46:13 +0200

    Hiding cart-dropdown component when go to checkout

--- clouth-store/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ clouth-store/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -7,8 +7,9 @@ import './cart-dropdown.styles.scss'
 import {selectCartItems} from "../../redux/cart/cart.selectors";
 import {createStructuredSelector} from "reselect";
 import { withRouter } from 'react-router-dom'
+import { toggleCartHidden } from "../../redux/cart/cart.actions";
 
-const CartDropdown = ({cartItems, history}) => (
+const CartDropdown = ({cartItems, history, dispatch}) => (
     cartItems.length ?
         (<div className='cart-dropdown'>
             <div className='cart-items'>
@@ -16,7 +17,12 @@ const CartDropdown = ({cartItems, history}) => (
                     cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem}/>)
                 }
             </div>
-            <CustomButton onClick={() => history.push('/checkout')}>CHECKOUT</CustomButton>
+            <CustomButton onClick={
+                () => {
+                    history.push('/checkout');
+                    dispatch(toggleCartHidden());
+                }
+            }>CHECKOUT</CustomButton>
         </div>)
         :
         (<div className='cart-dropdown'>
@@ -27,6 +33,10 @@ const CartDropdown = ({cartItems, history}) => (
 )
 
 const mapStateToProps = createStructuredSelector({cartItems: selectCartItems})
+
+const mapDispatchToProps = dispatch => ({
+    toggleCartHidden: () => dispatch(toggleCartHidden())
+});
 
 
 export default withRouter(connect(mapStateToProps)(CartDropdown));
