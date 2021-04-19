59628eccb6d85b4b6fcae80a5d0cf526b9ce4a72
Bartosz Tyrala <automationdream@gmail.com>
2021-04-19 22:03:48 +0200

    Collection state into Redux

--- clouth-store/src/pages/shop/shop.component.jsx
+++ clouth-store/src/pages/shop/shop.component.jsx
@@ -1,25 +1,21 @@
 import React from "react";
-import SHOP_DATA from "./shop.data";
+import {connect} from "react-redux";
+import {createStructuredSelector} from "reselect";
 import PreviewCollection from "../../components/preview-collection/preview-collection.component";
+import {selectCollections} from "../../components/shop/shop.selectors";
 
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-
-    render() {
-        const { collections }  = this.state;
-        return (<div className='shop-page'>
-            {
-                collections.map(({id, ...otherCollectionProps}) => (
+const ShopPage = ({collections}) => (
+    <div className='shop-page'>
+        {collections.map(({id, ...otherCollectionProps}) => (
                 <PreviewCollection key={id} {...otherCollectionProps} />
-                ))
-            }
-        </div>);
-    }
-}
+            ))}
+    </div>
+)
 
-export default ShopPage;
+const mapStateToProps = createStructuredSelector(
+    {
+        collections:selectCollections
+    }
+)
+
+export default connect(mapStateToProps)(ShopPage);
