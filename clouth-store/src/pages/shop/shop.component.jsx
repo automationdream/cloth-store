ea27efab31f9e6e1ac17dd3ec39ec67c76859483
Bartosz Tyrala <automationdream@gmail.com>
2021-04-27 14:59:58 +0200

    Routing CollectionsOverview and passing a match object into the route

--- clouth-store/src/pages/shop/shop.component.jsx
+++ clouth-store/src/pages/shop/shop.component.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import {Route} from 'react-router-dom'
 import CollectionsOverwiew from "../../components/collections-overview/collections-overview.component";
 
-const ShopPage = ({collections}) => (
-    <div className='shop-page'>
-        <CollectionsOverwiew/>
-    </div>
-)
+const ShopPage = ({match}) => {
+    console.log(match);
+    return (
+        <div className='shop-page'>
+            <Route exact path={`${match.path}`} component={CollectionsOverwiew}/>
+        </div>
+    )
+}
 
 
 export default ShopPage
