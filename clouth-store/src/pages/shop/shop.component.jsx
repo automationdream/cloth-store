b42d2dfceccf3ffdfb2a29810fa75b7014e8fb0c
Bartosz Tyrala <automationdream@gmail.com>
2021-04-27 15:34:29 +0200

    Adding the route to category component

--- clouth-store/src/pages/shop/shop.component.jsx
+++ clouth-store/src/pages/shop/shop.component.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import {Route} from 'react-router-dom'
 import CollectionsOverwiew from "../../components/collections-overview/collections-overview.component";
+import CategoryPage from "../category/category.component";
 
 const ShopPage = ({match}) => {
-    console.log(match);
     return (
         <div className='shop-page'>
             <Route exact path={`${match.path}`} component={CollectionsOverwiew}/>
+            <Route exact path={`${match.path}/:categoryId`} component={CategoryPage}/>
         </div>
     )
 }
