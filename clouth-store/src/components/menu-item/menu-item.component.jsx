f2279bbd96590c0de87201cbdd471d4deeb2ba1c
Bartosz Tyrala <automationdream@gmail.com>
2021-04-27 21:57:43 +0200

    Adding directory component with reducer

--- clouth-store/src/components/menu-item/menu-item.component.jsx
+++ clouth-store/src/components/menu-item/menu-item.component.jsx
@@ -3,13 +3,15 @@ import {withRouter} from 'react-router-dom';
 
 import './menu-item.styles.scss'
 
-const MenuItem = ({title, subtitle, imageURL, size, history, linkurl}) => (
+const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => (
 
-    <div className={`${size} menu-item`}>
-        <div style={{backgroundImage: `url(${imageURL})`}} className='background-image'/>
+    <div className={`${size} menu-item`}
+         onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
+        <div style={{backgroundImage: `url(${imageUrl})`}} className='background-image'/>
         <div className='content'>
             <h1 className='title'>{title.toUpperCase()}</h1>
-            <span className='subtitle'>{subtitle.toUpperCase()}</span>
+            <span className='subtitle'>SHOP NOW</span>
         </div>
     </div>
 );
--- clouth-store/src/redux/root-reducer.js
+++ clouth-store/src/redux/root-reducer.js
@@ -4,6 +4,7 @@ import storage from 'redux-persist/lib/storage';
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
 import shopReducer from "../components/shop/shop.reducer"
+import directoryReducer from "./directory/directory.reducer";
 
 
 const persistConfig = {
@@ -15,7 +16,8 @@ const persistConfig = {
 const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer,
-    shop: shopReducer
+    shop: shopReducer,
+    directory: directoryReducer
 });
 
 export default persistReducer(persistConfig, rootReducer);
